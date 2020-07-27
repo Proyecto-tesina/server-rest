96ef12f38da0483612fafe10eee1445225e5c95d
Andrés Milla <andressmilla@gmail.com>
2020-07-26 22:20:43 -0300

    Added API Schema documentation

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,24 +1,26 @@
-"""backend URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+
+from rest_framework.schemas import get_schema_view
+
+
+schema_view = get_schema_view(
+    title="DRT Simulator API",
+    description="API for manage statistics of the DRT simulation",
+    version="1.0.0"
+)
+
+api_doc_view = TemplateView.as_view(
+    template_name='doc.html',
+    extra_context={'schema_url': 'api-schema'}
+)
 
 
 urlpatterns = [
+    path('', api_doc_view, name='api-documentation'),
+    path('api/', schema_view, name='api-schema'),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('events/', include('events.urls'))
+    path('events/', include('events.urls')),
 ]
