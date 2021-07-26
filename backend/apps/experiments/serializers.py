d9fdbef629a280e9c01bdb09fcc8c9d09c40a8e1
Leonel <leonelmandarino@gmail.com>
2021-07-26 17:20:15 -0300

    Add unique event validator on event serializer

--- backend/apps/experiments/serializers.py
+++ backend/apps/experiments/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework import validators
 from rest_framework.serializers import ModelSerializer, ValidationError
 from .models import Event, Experiment
 
@@ -5,7 +6,15 @@ from .models import Event, Experiment
 class EventSerializer(ModelSerializer):
     class Meta:
         model = Event
-        fields = '__all__'
+        fields = "__all__"
+
+        validators = [
+            validators.UniqueTogetherValidator(
+                queryset=Event.objects.all(),
+                fields=["experiment", "name", "timestamp"],
+                message="Experiment, event name and timestamp are unique together",
+            )
+        ]
 
 
 class ExperimentSerializer(ModelSerializer):
@@ -13,15 +22,16 @@ class ExperimentSerializer(ModelSerializer):
 
     class Meta:
         model = Experiment
-        fields = '__all__'
+        fields = "__all__"
 
     def validate(self, data):
-        start_date = self._get_value(data, 'started_at')
-        end_date = self._get_value(data, 'ended_at')
+        start_date = self._get_value(data, "started_at")
+        end_date = self._get_value(data, "ended_at")
 
         if end_date and not start_date:
             raise ValidationError(
-                "If end_date was supplied, start date must be specified")
+                "If end_date was supplied, start date must be specified"
+            )
 
         if start_date and end_date and start_date > end_date:
             raise ValidationError("Start date must be previous to end date")
